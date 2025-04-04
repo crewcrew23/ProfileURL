87afd9c443faa8255aa2e82cb70d57a7d315c5ec
crewcrew23 <vasiliyiordanov23@gmail.com>
2025-04-04 15:42:26 +0300

    add jwt package

---
+++ internal/lib/jwt/jwt.go
@@ -0,0 +1,24 @@
+package jwt
+
+import (
+	"time"
+	"url_profile/internal/domain/models"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func NewToken(user *models.User, duration time.Duration, secret string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["uid"] = user.ID
+	claims["email"] = user.Email
+	claims["exp"] = time.Now().Add(duration).Unix()
+
+	tokenString, err := token.SignedString([]byte(secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
